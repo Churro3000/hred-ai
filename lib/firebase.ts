c3ec6c2a31563e6acd328dacc24f36ad07280009
Churro3000 <churro25200@gmail.com>
2026-04-01 22:10:08 +0200

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,20 @@ import { initializeApp, getApps } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
+const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY
+const authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
+const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_ID
+const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 }
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
