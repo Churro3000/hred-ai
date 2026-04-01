85cc9e66d64dc8f8cf9973fcf419746b8128ae44
Churro3000 <churro25200@gmail.com>
2026-04-01 21:21:50 +0200

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,31 +1,17 @@
-import { initializeApp, getApps, FirebaseApp } from 'firebase/app'
-import { getAuth, Auth } from 'firebase/auth'
-import { getFirestore, Firestore } from 'firebase/firestore'
+import { initializeApp, getApps } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
-let app: FirebaseApp
-let auth: Auth
-let db: Firestore
-
-const initFirebase = () => {
-  if (typeof window === 'undefined') return
-
-  if (!getApps().length) {
-    app = initializeApp({
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '',
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? '',
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? '',
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ?? '',
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_ID ?? '',
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? '',
-    })
-  } else {
-    app = getApps()[0]
-  }
-
-  auth = getAuth(app)
-  db = getFirestore(app)
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
-initFirebase()
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
 
-export { auth, db }
+export const auth = getAuth(app)
+export const db = getFirestore(app)
