4cae15768d5c21ce4c9d253cd0a7fa3115d6c8be
Churro3000 <churro25200@gmail.com>
2026-04-12 10:16:25 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['promptfoo'],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    }
+  }
 }
 module.exports = nextConfig
